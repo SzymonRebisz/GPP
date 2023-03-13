a2084ac34d12ced5d72bf0fb19f7a61f8c3d0d5f
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-13 23:51:57 +0100

    Added users models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=6)
+    town_name = models.CharField(max_length=32)
